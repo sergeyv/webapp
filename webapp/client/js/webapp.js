51ab4defbc0e11a183bc1b27c3a6720018cc5a8f
sergey.volobuev <sergey@mooball.com>
2011-06-09 09:27:25 +1000

    do not send formish sequence template back to server

--- webapp/client/js/webapp.js
+++ webapp/client/js/webapp.js
@@ -126,13 +126,16 @@
             var o = {},
                 a = this.serializeArray();
             $.each(a, function () {
-                if (o[this.name]) {
-                    if (!o[this.name].push) {
-                        o[this.name] = [o[this.name]];
+                // remove formish templates - the name contain *
+                if (this.name.indexOf('*') === -1) {
+                    if (o[this.name]) {
+                        if (!o[this.name].push) {
+                            o[this.name] = [o[this.name]];
+                        }
+                        o[this.name].push(this.value || '');
+                    } else {
+                        o[this.name] = this.value || '';
                     }
-                    o[this.name].push(this.value || '');
-                } else {
-                    o[this.name] = this.value || '';
                 }
             });
             return o;
