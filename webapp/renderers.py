499e86c20e6094a6809be2e8d00800ac9df45074
sergey.volobuev <sergey@mooball.com>
2011-06-29 15:19:44 +1000

    json renderer refactored

--- webapp/renderers.py
+++ webapp/renderers.py
@@ -7,23 +7,29 @@ from decimal import Decimal
 
 _JS_DATE_REGEXP = re.compile(r'"\*\*(new Date\([0-9,]+\))"')
 
+class _JSONDateEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime.datetime):
+            return obj.isoformat()
+
+        if isinstance(obj, datetime.date):
+            #return obj.strftime("%d %b %Y")
+            return obj.isoformat()
+
+        if isinstance(obj, Decimal):
+            return float(obj)
+
+        return json.JSONEncoder.default(self, obj)
+
+
+def safe_json_dumps(obj):
+    """
+    Improved version of json.dumps which support more datatypes
+    """
+    out = _JS_DATE_REGEXP.sub(r'\1', json.dumps(obj, cls=_JSONDateEncoder))
+    return unicode(out).decode('utf-8')
+
 def json_renderer_factory(name):
-
-    class _JSONDateEncoder(json.JSONEncoder):
-        def default(self, obj):
-            if isinstance(obj, datetime.datetime):
-                return obj.isoformat()
-
-            if isinstance(obj, datetime.date):
-                #return obj.strftime("%d %b %Y")
-                return obj.isoformat()
-
-            if isinstance(obj, Decimal):
-                return float(obj)
-
-            return json.JSONEncoder.default(self, obj)
-
-
 
     def _dumps(obj):
         """ A (simple)json wrapper that can wrap up python datetime and date
@@ -33,9 +39,7 @@ def json_renderer_factory(name):
 
         @returns: JSON version of the passed object
         """
-        out = _JS_DATE_REGEXP.sub(r'\1', json.dumps(obj, cls=_JSONDateEncoder))
-        return unicode(out).decode('utf-8')
-
+        return safe_json_dumps(obj)
 
     def _render(value, system):
         request = system.get('request')
