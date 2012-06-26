e73f8c88f72118026ce9f92f0bc1e61597c728d2
sergey.volobuev <sergey@mooball.com>
2012-06-26 14:21:55 +1000

    some logging removed

--- webapp/client/js/template.js
+++ webapp/client/js/template.js
@@ -198,11 +198,8 @@
 
 
         try {
-            //var started = Date.now()
             self.view.html($.jqote(self.template, {data: self.data, view: self}));
-            //console.log("View " + self.options.identifier + " rendered in " + (Date.now() - started) + " ms.");
         } catch (err) {
-            //alert(self.template.html());
             alert(err.message);
             if (!webapp.testmode) {
                 txt = "There was an error on this page.<br />"
